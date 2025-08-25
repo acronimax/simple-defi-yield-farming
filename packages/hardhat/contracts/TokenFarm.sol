fcd80ca18c0c83da1a252bf1220c2260d2276a1f
acronimax <dev@acronimax.io>
2025-08-24 18:54:00 -0600

    --update: update contract to add rewards and struct

--- packages/hardhat/contracts/TokenFarm.sol
+++ packages/hardhat/contracts/TokenFarm.sol
@@ -1,5 +1,5 @@
 // SPDX-License-Identifier: MIT
-pragma solidity ^0.8.18;
+pragma solidity >=0.8.0 <0.9.0;
 
 import "./DappToken.sol";
 import "./LPToken.sol";
@@ -14,22 +14,42 @@ contract TokenFarm {
     //
     string public name = "Proportional Token Farm";
     address public owner;
-    DappToken public dappToken;
+    DAppToken public dappToken;
     LPToken public lpToken;
     uint256 public constant REWARD_PER_BLOCK = 1e18; // Reward per block (total for all users)
     uint256 public totalStakingBalance; // Total tokens staked
     address[] public stakers;
+
+    // --- Variable Reward Rate ---
+    uint256 public rewardPerBlock;
+    uint256 public minRewardPerBlock;
+    uint256 public maxRewardPerBlock;
+    // ----------------------------
+
     mapping(address => uint256) public stakingBalance;
     mapping(address => uint256) public checkpoints;
     mapping(address => uint256) public pendingRewards;
     mapping(address => bool) public hasStaked;
     mapping(address => bool) public isStaking;
 
+    /**
+     * @notice Holds all data related to a single staker.
+     */
+    struct Staker {
+        uint256 stakingBalance;
+        uint256 checkpoint;
+        uint256 pendingRewards;
+        bool hasStaked;
+        bool isStaking;
+    }
+    mapping(address => Staker) public stakersInfo;
+
     // Events
     event Deposit(address indexed user, uint256 amount);
     event Withdraw(address indexed user, uint256 amount);
     event RewardsClaimed(address indexed user, uint256 amount);
     event RewardsDistributed(address indexed distributor);
+    event RewardRateChanged(uint256 oldRate, uint256 newRate);
 
     /**
      * @dev Throws if called by any account other than the owner.
@@ -52,10 +72,34 @@ contract TokenFarm {
      * @param _dappToken The address of the DappToken contract.
      * @param _lpToken The address of the LPToken contract.
      */
-    constructor(DappToken _dappToken, LPToken _lpToken) {
+    constructor(
+        DAppToken _dappToken,
+        LPToken _lpToken,
+        uint256 _initialReward,
+        uint256 _minReward,
+        uint256 _maxReward
+    ) {
+        require(_minReward <= _initialReward && _initialReward <= _maxReward, "Initial reward is out of range");
         dappToken = _dappToken;
         lpToken = _lpToken;
         owner = msg.sender;
+        rewardPerBlock = _initialReward;
+        minRewardPerBlock = _minReward;
+        maxRewardPerBlock = _maxReward;
+    }
+
+    /**
+     * @notice Allows the owner to update the reward rate per block.
+     * @param _newRate The new reward rate to be set.
+     */
+    function setRewardPerBlock(uint256 _newRate) external onlyOwner {
+        require(
+            _newRate >= minRewardPerBlock && _newRate <= maxRewardPerBlock,
+            "New rate is outside the allowed range"
+        );
+        uint256 oldRate = rewardPerBlock;
+        rewardPerBlock = _newRate;
+        emit RewardRateChanged(oldRate, _newRate);
     }
 
     /**
@@ -64,24 +108,24 @@ contract TokenFarm {
      */
     function deposit(uint256 _amount) external {
         require(_amount > 0, "Amount must be greater than 0");
+        Staker storage user = stakersInfo[msg.sender];
 
         // Distribute any pending rewards before updating the staking balance
-        if (isStaking[msg.sender]) {
+        if (user.isStaking) {
             distributeRewards(msg.sender);
         }
 
         lpToken.transferFrom(msg.sender, address(this), _amount);
-        stakingBalance[msg.sender] += _amount;
+        user.stakingBalance += _amount;
         totalStakingBalance += _amount;
 
-        if (!hasStaked[msg.sender]) {
+        if (!user.hasStaked) {
             stakers.push(msg.sender);
-            hasStaked[msg.sender] = true;
+            user.hasStaked = true;
         }
 
-        isStaking[msg.sender] = true;
-        checkpoints[msg.sender] = block.number; // Set checkpoint to the current block
-
+        user.isStaking = true;
+        user.checkpoint = block.number;
         emit Deposit(msg.sender, _amount);
     }
 
@@ -89,19 +133,17 @@ contract TokenFarm {
      * @notice Withdraws all staked LP tokens.
      */
     function withdraw() external {
-        require(isStaking[msg.sender], "You are not staking");
-        uint256 balance = stakingBalance[msg.sender];
-        require(balance > 0, "Staking balance must be greater than 0");
+        Staker storage user = stakersInfo[msg.sender];
+        uint256 balance = user.stakingBalance;
 
-        // Calculate and update pending rewards before withdrawal
+        require(balance > 0, "Staking balance must be greater than 0");
         distributeRewards(msg.sender);
 
         totalStakingBalance -= balance;
-        stakingBalance[msg.sender] = 0;
-        isStaking[msg.sender] = false;
+        user.stakingBalance = 0;
+        user.isStaking = false;
 
         lpToken.transfer(msg.sender, balance);
-
         emit Withdraw(msg.sender, balance);
     }
 
@@ -112,12 +154,12 @@ contract TokenFarm {
         // Distribute rewards one last time before claiming
         distributeRewards(msg.sender);
 
-        uint256 pendingAmount = pendingRewards[msg.sender];
+        Staker storage user = stakersInfo[msg.sender];
+        uint256 pendingAmount = user.pendingRewards;
         require(pendingAmount > 0, "No rewards to claim");
 
-        pendingRewards[msg.sender] = 0;
+        user.pendingRewards = 0;
         dappToken.mint(msg.sender, pendingAmount);
-
         emit RewardsClaimed(msg.sender, pendingAmount);
     }
 
@@ -126,9 +168,9 @@ contract TokenFarm {
      */
     function distributeRewardsAll() external onlyOwner {
         for (uint i = 0; i < stakers.length; i++) {
-            address user = stakers[i];
-            if (isStaking[user]) {
-                distributeRewards(user);
+            address userAddress = stakers[i];
+            if (stakersInfo[userAddress].isStaking) {
+                distributeRewards(userAddress);
             }
         }
         emit RewardsDistributed(msg.sender);
@@ -140,13 +182,15 @@ contract TokenFarm {
      * since their last checkpoint.
      */
     function distributeRewards(address beneficiary) private {
-        if (totalStakingBalance > 0 && block.number > checkpoints[beneficiary]) {
-            uint256 blocksPassed = block.number - checkpoints[beneficiary];
+        Staker storage user = stakersInfo[beneficiary];
+        if (totalStakingBalance > 0 && block.number > user.checkpoint) {
             // Calculate reward using precise multiplication and division
-            uint256 reward = (REWARD_PER_BLOCK * blocksPassed * stakingBalance[beneficiary]) / totalStakingBalance;
-            pendingRewards[beneficiary] += reward;
+            uint256 blocksPassed = block.number - user.checkpoint;
+            // The calculation now uses the state variable instead of the constant
+            uint256 reward = (rewardPerBlock * blocksPassed * user.stakingBalance) / totalStakingBalance;
+            user.pendingRewards += reward;
         }
         // Update the checkpoint to the current block for the next calculation
-        checkpoints[beneficiary] = block.number;
+        user.checkpoint = block.number;
     }
 }
